f0dee1b79276f1304f47cf559849ef79b98350ff
Ethan Steere <62357808+e253@users.noreply.github.com>
2023-11-21 12:58:43 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Cowsay from './Cowsay.svelte'
+</script>
+
+<main class="container">
+  <h1>What should the Cow Say?</h1>
+
+  <div class="row">
+    <img src="/icon.png" class="logo tauri" alt="Tauri Logo" />
+  </div>
+
+  <div class="row">
+    <Cowsay />
+  </div>
+</main>
