c4780664123781d8d09f0071626490b0eb6dec22
Ethan Steere <62357808+e253@users.noreply.github.com>
2023-12-05 00:49:55 -0500

    updater integrated

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,34 @@
 <script lang="ts">
+  import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
+  import { relaunch } from '@tauri-apps/api/process'
+  import { onMount } from 'svelte'
+
+  let updateState: 'noUpdates' | 'updateAvailable' | 'updateDownloading' | 'restarting' = "noUpdates";
+
+  onMount(async () => {
+    try {
+      const { shouldUpdate, manifest } = await checkUpdate()
+
+      if (shouldUpdate) {
+        updateState = 'updateAvailable'
+        // You could show a dialog asking the user if they want to install the update here.
+        console.log(
+          `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
+        )
+        updateState = 'updateDownloading';
+        // Install the update. This will also restart the app on Windows!
+        await installUpdate()
+
+        updateState = 'restarting'
+        // On macOS and Linux you will need to restart the app manually.
+        // You could use this step to display another confirmation dialog.
+        await relaunch()
+      }
+    } catch (error) {
+      console.error(error)
+    }  
+  })
+
   let name = ""
   let greetMsg = ""
 </script>
@@ -25,3 +55,15 @@
     </form>
   </div>
 </main>
+
+<div class="container update-status">
+  {#if updateState === "noUpdates"}
+    <p>No Updates Available</p>
+  {:else if updateState === "updateAvailable"}
+    <p>Update Available!</p>
+  {:else if updateState === "updateDownloading"}
+    <p>Update is Beind Downloaded</p>
+  {:else}
+    <p>Restarting Application</p>
+  {/if}
+</div>
